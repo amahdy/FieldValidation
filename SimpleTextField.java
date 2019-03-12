af6dfb85628867dec01806b9059e1a48deb6e374
A.Mahdy AbdelAziz <amahdy@users.noreply.github.com>
2019-03-12 09:42:00 +0200

    Create SimpleTextField.java

---
+++ SimpleTextField.java
@@ -0,0 +1,9 @@
+package org.vaadin.amahdy.fieldvalidation;
+
+class SimpleTextField extends GenericTextField<String> {
+
+  /**
+   * Special and more common case.
+   * Nothing much to add here.
+   */
+}
